6366747bbca306ef269d1c5048142a1b856b41f7
oramos510 <oramos.cmma@gmail.com>
2024-11-23 09:52:56 -0800

    W01 Prove

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
+            {
+                break;
+            }
+            numbers.Add(input);
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+        int smallestPositive = numbers.Where(n => n > 0).DefaultIfEmpty(int.MaxValue).Min();
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
